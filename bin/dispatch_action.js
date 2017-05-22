c2b5123028c2e4b61c338bfa222dee9032540f03
Dre_ <andre.garvin@envirostudies.org>
2017-05-22 14:44:40 +0000

    added readme.md

--- bin/dispatch_action.js
+++ bin/dispatch_action.js
@@ -21,40 +21,41 @@ module.exports.dispatch_action = (method, obj, commands) => {
               // checks wether the command exists
               if ( tool.includes(Object.keys( commands ), obj.action ) ) {
 
-                  var command = commands[ obj.action ];
+                    var command = commands[ obj.action ];
+                
+                    console.log();
+                // checks weather the command requires any arguements or paylaod
+                //   if ( command.args === false && obj.args.length > 0 ) {
 
-                  // checks weather the command requires any arguements or paylaod
-                  if ( command.args === false && obj.args.length > 0 ) {
+                //       return resp = `*error: Command '${ obj.action }' takes no arguments`;
+                //   }
+                //   else if ( command.payload === false && obj.payload.length > 0 ) {
 
-                      return resp = `*error: Command '${ obj.action }' takes no arguments`;
-                  }
-                  else if ( command.payload === false && obj.payload.length > 0 ) {
-
-                      resp = `*error: Command '${ obj.action }' takes no arguments.`;
-                  }
+                //       resp = `*error: Command '${ obj.action }' takes no arguments.`;
+                //   }
 
 
-                  // if the user givens no arguements or payload
-                  if ( obj.args.length === 0 || obj.payload.length === 0 ) {
+                //   // if the user givens no arguements or payload
+                //   if ( obj.args.length === 0 || obj.payload.length === 0 ) {
 
-                      resp = `*error: '${ obj.action }' must receive arguments and payload`;
-                  }
+                //       resp = `*error: '${ obj.action }' must receive arguments and payload`;
+                //   }
 
-                  // checks none of the arguements given are acyaul arguemts
-                  for ( var j in obj.args ) {
+                //   // checks none of the arguements given are acyaul arguemts
+                //   for ( var j in obj.args ) {
 
-                      /*
-                          includes return 'true' as a boolean value
-                          so by using '!' if it returns 'true' that will turn into false
-                          but if it retuns 'false' that with turn that into 'true'
-                      */
-                      if ( !tool.includes(commnads.args, obj.args[ j ]) ) {
+                //       /*
+                //           includes return 'true' as a boolean value
+                //           so by using '!' if it returns 'true' that will turn into false
+                //           but if it retuns 'false' that with turn that into 'true'
+                //       */
+                //       if ( !tool.includes(commands.args, obj.args[ j ]) ) {
 
-                          return resp = `*error: Unknown arguement '${ obj.args[j] }'.`;
-                      }
-                  }
+                //           return resp = `*error: Unknown arguement '${ obj.args[j] }'.`;
+                //       }
+                //   }
 
-                  return resp !== undefined ? resp : resp = 1;
+                //   return resp !== undefined ? resp : resp = 1;
 
                   // if ( ( obj.args.length > 0 && globalState.commands[obj.action].args === true )
                   // || ( globalState.commands[obj.action] === true && obj.pyload.length > 0 ) ) {
