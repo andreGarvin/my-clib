a98407286ad6938270fb6a5782910b931ae78329
Dre_ <andre.garvin@envirostudies.org>
2017-05-28 20:00:50 +0000

    improved the exArsg and placed inisde of the index.js file on the module.exports object.

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ module.exports = {
     argParser: require('./arg-parser.js').argParser,
     dispatch_action: require('./dispatch_action.js').dispatch_action,
     help: require('./help.js').manual,
-    global_state: ''
+    global_state: '',
+    exArgs: function( arr, method, data ) {
+    
+        /*
+            assign data to output so when it passes it to method
+            on the first run it does not refeence the smae first 
+            vlaue and rsssings to a new value
+        */
+        var output = data;
+        for ( var i = 0; i < arr.length; i++ ) {
+            
+            // call the mwthod again nd pass in the data
+            output = method(arr[i], output);
+        }
+    
+        return output;
+    }
 };
