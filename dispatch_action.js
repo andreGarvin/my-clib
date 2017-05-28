8c3c477ac9e7e8a9cff609aae453cc1c88550ec4
Dre_ <andre.garvin@envirostudies.org>
2017-05-28 00:38:58 +0000

    uploading the lastest changes to bin the node module

---
+++ dispatch_action.js
@@ -0,0 +1,45 @@
+const tool = require('./tools.js'),
+help = require('./help.js'),
+path = require('path'),
+fs = require('fs');
+
+// dispatchs the actions to the
+module.exports.dispatch_action = (method, obj, commands) => {
+
+    var resp = 0;
+    switch ( obj.action ) {
+        case 'help':
+
+            //   if ( obj.payload.length !== 0 )
+            //       help.manual(obj.pyload);
+
+            //   help.manual();
+            break;
+
+        default:
+
+              // checks wether the command exists
+              if ( tool.includes(Object.keys( commands ), obj.action ) ) {
+
+                    // checks weather the command requires any arguements or paylaod
+                    var command = commands[ obj.action ];
+                    if ( ( obj.payload.length !== 0 && command.payload === null ) || ( obj.args.length !== 0 && command.args === null ) ) {
+                        
+                        resp = '*error: This command does accept any payload or arguments.';
+                    }
+              }
+              else {
+                  
+                  resp = '*error: This command does not exist.';
+              }
+              break;
+
+     }
+     
+     if ( resp !== 0 ) {
+        
+         return tool.catchError( resp );
+     }
+    
+     return method(obj.action, obj);
+};
