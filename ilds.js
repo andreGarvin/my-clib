963a3692d26596bb66c3f2a1fd4f70af2cbd5b0c
andreGarvin <andregarvin718@gmail.com>
2017-05-30 18:24:36 -0400

    saving changes made to ilds.js save() function

--- ilds.js
+++ ilds.js
@@ -1,4 +1,5 @@
 const fs = require('fs'),
+path = require('path'),
 tool = require('./tools.js');
 
 // loads the json file
@@ -18,22 +19,40 @@ exports.onload = function( file_name, callback ) {
               // using the internal tool
               return tool.catchError(err.message, callback === undefined ? undefined : callback);
             }
-            
+
             // if the data is not empty load it the object property 'global_state' or wherever
             if ( data === undefined || data === null || data.length === 0 ) {
-                
+
                 return tool.catchError(`*error: No data was able to be retreived from ${ file_name }.`, callback === undefined ? undefined : callback);
             }
-              
+
             return callback === undefined ? data : callback( null, data );
           });
 
         })
     }
     catch (err) {
-        
+
         // if there is a error the return catchError() with a callback{} if one as provided.
         return tool.catchError(err, callback === undefined ? undefined : callback);
     }
+};
 
-}
+
+module.exports.save = (path, data, callback) => {
+
+      path = path.resolve(`${ __dirname }${ path }`);
+      fs.writeFile(path, data, (err) => {
+          if (err) return tool.catchError(err);
+
+          if ( callback !== undefined ) {
+
+              return callback(null, {
+                  msg: `${ typeof data === string ? data.length : 'inputed data' } was saved to '${ path }.'`,
+                  status: true
+              });
+          }
+
+          return console.log(`${ typeof data === string ? data.length : 'data' } was saved to '${ path }'`);
+      });
+};
