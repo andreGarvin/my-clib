e7e6d47f9a585e52e56e05606dcae5d54f6a29d6
Dre_ <andre.garvin@envirostudies.org>
2017-06-21 22:23:09 +0000

    small change to the ilds.js file making the functions more dynamic to run sync or async. Later will target logger and other functions with horribly written code I had written to make them easier to look at.

--- ilds.js
+++ ilds.js
@@ -1,58 +1,59 @@
+'use strict';
+
 const fs = require('fs'),
 path = require('path'),
 tool = require('./tools.js');
 
 // loads the json file
 exports.onload = function( file_name, callback ) {
-
-    // tries the script
+    
     try {
+        file_name = path.resolve( file_name );
+    
+    
+        let exst = fs.existsSync(file_name);
         // checks if the file exist
-        fs.exists(file_name, (exst) => {
-          if (!exst) return tool.catchError(`*error: '${ file_name }' does not exist`, callback === undefined ? undefined : callback);
-
-          //  if the data exist/JSON file is then load the data and parse the JSON data
-          fs.readFile(file_name, 'utf8', (err, data) => {
-            if (err) {
-
-              // if the err occured then return the err
-              // using the internal tool
-              return tool.catchError(err.message, callback === undefined ? undefined : callback);
-            }
-
+        if ( exst ) {
+            
+            var data = fs.readFileSync(file_name, 'utf8');
+        
             // if the data is not empty load it the object property 'global_state' or wherever
             if ( data === undefined || data === null || data.length === 0 ) {
 
-                return tool.catchError(`*error: No data was able to be retreived from ${ file_name }.`, callback === undefined ? undefined : callback);
+                return tool.catchError(`*error: No data was able to be retreived from ${ file_name }.`, callback === undefined ? undefined : callback)
             }
 
-            return callback === undefined ? data : callback( null, data );
-          });
-
-        })
+            return callback === undefined ? data : callback( null, data )
+        }
+        else {
+            
+            return return tool.catchError(`*error: File path '${ file_name }' does not exist.`, callback === undefined ? undefined : callback){
+        }
     }
     catch (err) {
 
         // if there is a error the return catchError() with a callback{} if one as provided.
-        return tool.catchError(err, callback === undefined ? undefined : callback);
+        return tool.catchError(err, callback === undefined ? undefined : callback)
     }
+    
 };
 
 
 module.exports.save = (path, data, callback) => {
 
-      path = path.resolve(`${ __dirname }${ path }`);
-      fs.writeFile(path, data, (err) => {
-          if (err) return tool.catchError(err);
+    try {
+        
+        path = path.resolve( path );
+    
+        fs.writeFile(path, data);
 
-          if ( callback !== undefined ) {
-
-              return callback(null, {
-                  msg: `${ typeof data === 'string' ? data.length : 'inputed data' } was saved to '${ path }.'`,
-                  status: true
-              });
-          }
-
-          return console.log(`${ typeof data === 'string' ? data.length : 'data' } was saved to '${ path }'`);
-      });
+        return callback !== undefined ? callback(null, {
+            msg: `${ typeof data === 'string' ? data.length : 'inputed data' } was saved to '${ path }.'`,
+            status: true
+        }) : console.log(`${ typeof data === 'string' ? data.length : 'data' } was saved to '${ path }'`)
+    }
+    catch (e) {
+         
+        return tool.catchError(err, callback === undefined ? undefined : callback);
+    }
 };
