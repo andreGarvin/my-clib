c777864195474fe6a7b10ea6f48bd9fa96e415de
andreGarvin <andregarvin718@gmail.com>
2017-07-07 19:09:09 -0400

    edited the .gitignore file to hide more mthods to build later also made a small edited on the ilds.js file just a line protion of the onload() method.

--- ilds.js
+++ ilds.js
@@ -6,27 +6,27 @@ tool = require('./tools.js');
 
 // loads the json file
 exports.onload = function( file_name, callback ) {
-    
+
     try {
         file_name = path.resolve( file_name );
-    
-    
+
+
         let exst = fs.existsSync(file_name);
         // checks if the file exist
         if ( exst ) {
-            
+
             var data = fs.readFileSync(file_name, 'utf8');
-        
+
             // if the data is not empty load it the object property 'global_state' or wherever
             if ( data === undefined || data === null || data.length === 0 ) {
 
                 return tool.catchError(`*error: No data was able to be retreived from ${ file_name }.`, callback === undefined ? undefined : callback)
             }
 
-            return callback === undefined ? data : callback( null, data )
+            return callback === undefined ? data : callback( data )
         }
         else {
-            
+
             return tool.catchError(`*error: File path '${ file_name }' does not exist.`, callback === undefined ? undefined : callback)
         }
     }
@@ -35,16 +35,16 @@ exports.onload = function( file_name, callback ) {
         // if there is a error the return catchError() with a callback{} if one as provided.
         return tool.catchError(err, callback === undefined ? undefined : callback)
     }
-    
+
 };
 
 
 module.exports.save = (path, data, callback) => {
 
     try {
-        
+
         path = path.resolve( path );
-    
+
         fs.writeFile(path, data);
 
         return callback !== undefined ? callback(null, {
@@ -53,7 +53,7 @@ module.exports.save = (path, data, callback) => {
         }) : console.log(`${ typeof data === 'string' ? data.length : 'data' } was saved to '${ path }'`)
     }
     catch (e) {
-         
+
         return tool.catchError(err, callback === undefined ? undefined : callback);
     }
 };
